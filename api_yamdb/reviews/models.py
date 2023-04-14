fcdda72a069547a5e5870cfd9e2dd93259a48b8e
Porechnyi Denis <Pro_15.6_6@outlook.com>
2023-04-14 18:05:37 +0700

    Add admin site registry, update models category, genre, title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -13,6 +14,13 @@ class Genre(models.Model):
         verbose_name='Слаг жанра'
     )
 
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name[:settings.DISP_LETTERS]
+
 
 class Title(models.Model):
     name = models.CharField(
@@ -22,34 +30,50 @@ class Title(models.Model):
         verbose_name='Название произведения'
     )
     year = models.IntegerField()
-    description = models.TextField()
-    genre = models.ManyToManyField(
-        through='GenreTitle',
-        on_delete=models.SET_NULL,
+    description = models.TextField(
         blank=True,
         null=True,
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreTitle',
+        through_fields=('title', 'genre'),
         related_name='titles',
         verbose_name='Жанр'
     )
     category = models.ForeignKey(
         'Category',
         on_delete=models.SET_NULL,
-        null=True,
+
         blank=True,
-        related_name='titles'
+        null=True,
+        related_name='titles',
+        verbose_name='Категория'
     )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Приозведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name[:settings.DISP_LETTERS]
 
 
 class GenreTitle(models.Model):
     genre = models.ForeignKey(
         Genre,
         related_name='genre_title',
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
     )
     title = models.ForeignKey(
         Title,
         related_name='genre_title',
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
     )
 
     class Meta:
@@ -59,6 +83,9 @@ class GenreTitle(models.Model):
                 name='unique_genre_title'
             ),
         ]
+
+    def __str__(self):
+        return f'{self.title.id} - {self.genre.id}'
 
 
 class Category(models.Model):
@@ -72,3 +99,11 @@ class Category(models.Model):
         unique=True,
         verbose_name='Слаг категории'
     )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name[:settings.DISP_LETTERS]
