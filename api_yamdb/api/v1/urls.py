d69f7c1495bb621d3ddcfbb7fb12397765edc00c
Денис Бобенко <bbenko01@yandex.ru>
2023-04-20 14:59:04 +0300

    add API versioning

---
+++ api_yamdb/api/v1/urls.py
@@ -0,0 +1,30 @@
+from django.urls import include, path
+from .views import (CategoryViewSet, GenreViewSet, TitleViewSet,
+                    create_token, signup, UserViewSet, CommentViewSet,
+                    ReviewViewSet)
+from rest_framework.routers import DefaultRouter
+
+app_name = 'v1'
+
+v1_router = DefaultRouter()
+
+v1_router.register(r'users', UserViewSet, basename='user')
+v1_router.register(r'categories', CategoryViewSet, basename='category')
+v1_router.register(r'genres', GenreViewSet, basename='genre')
+v1_router.register(r'titles', TitleViewSet, basename='title_id')
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comments'
+)
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet, basename='reviews'
+)
+
+urlpatterns = [
+    path('', include(v1_router.urls)),
+    path('auth/', include([
+        path('token/', create_token),
+        path('signup/', signup)
+    ]))
+]
